01a90f402359efbad7fbd05401fd417e25d424dd
maiky5198 <maiky5198@gmail.com>
2022-06-25 18:47:12 -0400

    constant time and more notes

--- Big_o_scalability.js
+++ Big_o_scalability.js
@@ -1,17 +1,19 @@
-// const nemo = ['nemo'];
+//NOTES learning BIG O
+const nemo = ['nemo'];
 
-// function findNemo(array) {
-//     for (let i = 0; i < array.length; i++) {
-//         if (array[i] === 'nemo') {
-//             console.log('Found nemo!');
-//         }
-//     }
-// }
+function findNemo(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 'nemo') {
+            console.log('Found nemo!');
+        }
+    }
+}
 
-// findNemo(nemo);
+findNemo(nemo);
 
 
 //NOTES here we are going to see how to scale an array and review its performance to later optimize the array
+//NOTES the function depends on the number of inputs in the array. So time to exucute is linear
 const nemo = ['nemo'];
 const animals = ['dog', 'cat', 'fish']
 const large = new Array(100).fill('nemo')
@@ -28,3 +30,14 @@ function findNemo(array) {
 }
 
 findNemo(large); // O(n) --> linear time
+
+
+// NOTES example of Constant Time, the input size doesnt matter, each time the function will run once
+const boxes = [0,1,2,3,4,5];
+
+function logFirstTwoBoxes(boxes) {
+    console.log(boxes[0]); //O(1)
+    console.log(boxes[1]); //O(1)
+}
+
+logFirstTwoBoxes(boxes) //O(2)

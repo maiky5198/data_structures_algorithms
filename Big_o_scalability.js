c6df53751a851c61f7452b827fdc2fd3d630c415
maiky5198 <maiky5198@gmail.com>
2022-06-25 20:06:38 -0400

    Big O rules

--- Big_o_scalability.js
+++ Big_o_scalability.js
@@ -12,10 +12,10 @@ function findNemo(array) {
 findNemo(nemo);
 
 
-//NOTES here we are going to see how to scale an array and review its performance to later optimize the array
-//NOTES the function depends on the number of inputs in the array. So time to exucute is linear
+// NOTES here we are going to see how to scale an array and review its performance to later optimize the array
+// NOTES the function depends on the number of inputs in the array. So time to exucute is linear
 const nemo = ['nemo'];
-const animals = ['dog', 'cat', 'fish']
+const animals = ['dog', 'cat', 'fish', 'nemo', 'cheetah']
 const large = new Array(100).fill('nemo')
 
 function findNemo(array) {
@@ -32,7 +32,7 @@ function findNemo(array) {
 findNemo(large); // O(n) --> linear time
 
 
-// NOTES example of Constant Time, the input size doesnt matter, each time the function will run once
+//NOTES example of Constant Time, the input size doesnt matter, each time the function will run once
 const boxes = [0,1,2,3,4,5];
 
 function logFirstTwoBoxes(boxes) {
@@ -41,3 +41,61 @@ function logFirstTwoBoxes(boxes) {
 }
 
 logFirstTwoBoxes(boxes) //O(2)
+
+//NOTES calculating BIG O 
+
+function funChallenge(input) {
+    let a = 10; //O(1)
+    a = 50 + 3; //O(1)
+
+    for (let i = 0; i < input.length; i++) { //O(n)
+        anotherFunction(); //O(n)
+        let stranger = true; //O(n)
+        a++; //O(n)
+    }
+    return a; //O(1)
+}
+BIG O(3 +4n)
+
+//NOTES calculating Big O
+function anotherFunChallenge(input) {
+    let a = 5;// O(1)
+    let b = 10;// O(1)
+    let c = 50;// O(1)
+    for (let i = 0; i < input; i++) {// O(n)
+      let x = i + 1;// O(n)
+      let y = i + 2;// O(n)
+      let z = i + 3;// O(n)
+  
+    }
+    for (let j = 0; j < input; j++) {// O(n)
+      let p = j * 2;// O(n)
+      let q = j * 2;// O(n)
+    }
+    let whoAmI = "I don't know";// O(1)
+  }
+  
+// NOTES 4 constant 5 linear
+BIG O(4 + 5n)
+
+
+
+//NOTES BIG O RULE 1  WE ALWAYS CARE ABOUT WORST CASE SCENRIO  
+//NOTES adjusting performance 
+const nemo = ['nemo'];
+const animals = ['dog', 'cat', 'fish', 'nemo', 'cheetah']
+
+function findNemo(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 'nemo') {
+            console.log('Found Nemo!');
+            //as soon as it finds nemo the loop ends
+            break;
+        }
+    }
+}
+
+findNemo(animals); // O(n) --> linear time
+
+//
+  
